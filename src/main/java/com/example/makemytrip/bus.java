4a5caba04c281b47a0668d2cb654ef4af4330d6e
unknown <viraj.pophale.0412@gmail.com>
2025-04-25 19:16:00 +0530

    adding new feature

--- src/main/java/com/example/makemytrip/bus.java
+++ src/main/java/com/example/makemytrip/bus.java
@@ -4,7 +4,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class flights {
-    @GetMapping("/flights")
+public class bus {
+    @GetMapping("/bus")
     public String getData() {return  "Please book your bus tickets from MMT kindly book ticket for New Delhi to anywhere at 10% discount" ; }
 }
