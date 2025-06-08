7dcb6024ee6e0256ec723105244cac3f7d6ffc8b
unknown <viraj.pophale.0412@gmail.com>
2025-06-08 16:14:39 +0530

    adding new feature cexchange

--- src/main/java/com/example/makemytrip/cexchange.java
+++ src/main/java/com/example/makemytrip/cexchange.java
@@ -6,5 +6,5 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class cexchange {
     @GetMapping("/cexchange")
-    public String getData() {return  "Please exchange your currency at Airport through MMT and get 5% discount on any hotel reservations at your destination" ; }
+    public String getData() {return  "Please exchange your currency at Airport through MMT and get 10% discount on any hotel reservations at your destination" ; }
 }
